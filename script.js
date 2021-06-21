464e518091e808a278a78904c560b56caecd64da
ftanevski <tanevski.f98@gmail.com>
2021-06-21 12:53:21 +0200

    Tweaked cards CSS

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-function createCard(){
-    let card = document.createElement("div");
-    card.id = "card"
-    let cardHeader = document.createElement("div");
-    cardHeader.innerHTML = "<img src=" + profile_image + ">/img>"
-}
-
 let xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -15,32 +8,40 @@ xhttp.onreadystatechange = function() {
             let caption = "<p id=\"caption\">" + response[i].caption + "</p>";
             let source = "<a href=" + response[i].source_link + "><img class=\"social\" src=/icons/" + response[i].source_type + ".svg></img></a>"  
             let date = response[i].date;
-            let likes = "<p class=\"likes\">" + response[i].likes + "</p>";
+            let likes = "<span class=\"likes\">" + response[i].likes + "</span>";
             let name = "<b class=\"name\">" + response[i].name + "</b>";
             let profile_image = "<img class=\"profileImage\" src=" + response[i].profile_image + "></img>";
             let heart = "<img class=\"heartIcon\" src=\"/icons/heart.svg\"></img>"
-            var card = document.createElement("div");
-            card.id = "card";
-            var cardHeader = document.createElement("div");
-            cardHeader.id = "cardHeader";
-            cardHeader.innerHTML = profile_image + " " + name + " " + date + " " + source;
+            let card = document.createElement("div");
+            card.className = "card";
+            let cardHeader = document.createElement("div");
+            let profile = document.createElement("div");
+            profile.className = "profile";
+            profile.innerHTML = "<div>" + profile_image + "</div>";
+            let nameContainer = document.createElement("div");
+            nameContainer.className = "nameContainer";
+            nameContainer.innerHTML = "<span>" + name + "</span><span>" + date + "</span>" 
+            cardHeader.className = "cardHeader";
+            profile.append(nameContainer);
+            cardHeader.append(profile);
+            cardHeader.innerHTML += "<div>" + source + "</div>";
             card.append(cardHeader);
-            var cardImage = document.createElement("div");
-            cardImage.id = "cardImage";
+            let cardImage = document.createElement("div");
+            cardImage.className = "cardImage";
             cardImage.innerHTML = image;
             card.append(cardImage);
-            var cardCaption = document.createElement("div");
-            var hr = document.createElement("hr");
-            cardCaption.id = "cardCaption";
+            let cardCaption = document.createElement("div");
+            let hr = document.createElement("hr");
+            cardCaption.className = "cardCaption";
             cardCaption.innerHTML = caption;
             card.append(cardCaption);
             card.append(hr);
-            var cardFooter = document.createElement("div");
-            cardFooter.id = "cardFooter";
+            let cardFooter = document.createElement("div");
+            cardFooter.className = "cardFooter";
             cardFooter.innerHTML = heart + " " + likes;
             card.append(cardFooter);
             container.append(card);
-            // var el = document.createElement("div");
+            // let el = document.createElement("div");
             // el.innerHTML = profile_image + " " + name + " " + date + " " + source + " " + image + " " + caption + "<hr>" + heart + " " + likes;
             // el.id = "card";
             // container.append(el);
